581a62d063e0055a4e4ece4d2cf2fff8f51ec636
Praveen Kumar Pandey <praveen-kumar.pandey@hpe.com>
2019-03-12 23:15:43 +0530

    inital commit

---
+++ public/js/app.js
@@ -0,0 +1,47 @@
+// console.log("client side javascript file loaded");
+
+// fetch("http://puzzle.mead.io/puzzle").then(response => {
+//   response.json().then(data => {
+//     console.log(data);
+//   });
+// });
+
+// fetch("http://localhost:3000/weather?address=Bangalore").then(response => {
+//   response.json().then((data)=> {
+//       console.log('test')
+    //if (data.error) {
+    //   console.log(data.error);
+    // } else {
+    //   console.log(data.forecast);
+    //   console.log(data.location);
+    // }
+//   });
+// });
+console.log('Client side javascript file is loaded!')
+
+
+const weatherForm= document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne=document.querySelector('#message-1')
+const messageTwo=document.querySelector('#message-2')
+messageOne.textContent='test'
+
+weatherForm.addEventListener('submit',(e)=>{
+   e.preventDefault()
+   const location=search.value;
+   messageOne.textContent='Loading'
+   fetch('http://localhost:3000/weather?address='+location).then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            messageOne.textContent=data.error;
+            console.log(data.error)
+        } else {
+            messageOne.textContent=data.location
+            messageTwo.textContent=data.forecast
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+})
+
+})
---
+++ src/app.js
@@ -0,0 +1,98 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+const geoCode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+
+//Define path for express
+const publicDirPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+console.log(partialsPath);
+const app = express();
+//Set express config
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+//Define express static directory
+app.use(express.static(publicDirPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "weather app",
+    name: "Praveen"
+  });
+});
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "about",
+    name: "Praveen"
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "help",
+    message: "How can i help you?",
+    name: "Praveen"
+  });
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "you must provide address"
+    });
+  }
+  geoCode(req.query.address, (error, { latitude, longitude, location }={}) => {
+    if (error) {
+      return res.send({ error });
+    }
+    forecast(latitude, longitude, (error, forecastData) => {
+      if(error){
+       return res.send({error})
+      }
+      console.log(latitude, longitude,forecastData)
+      res.send({
+        forecast:forecastData,
+        location,
+        address:req.query.address
+      })
+    });
+  });
+
+  // res.send({
+  //   forecast: "It currently 36 degrees and there is no chance of rain",
+  //   location: req.query.address
+  // });
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "you must provide search value"
+    });
+  }
+  console.log(req.query.search);
+  res.send({
+    products: []
+  });
+});
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    name: "Praveen",
+    title: "404",
+    errorMessage: "help doc not found"
+  });
+});
+app.get("*", (req, res) => {
+  res.render("404", {
+    name: "Praveen",
+    title: "404",
+    errorMessage: "page not found"
+  });
+});
+app.listen("3000", () => {
+  console.log("server is up");
+});
